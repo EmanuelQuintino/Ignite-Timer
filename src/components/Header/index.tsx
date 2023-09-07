b1b262138ef2b7ee69f636db27db786827d2174e
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-06 23:42:59 -0300

    add styles component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,20 @@
 import { Container } from "./style";
+import logoIgnite from "../../assets/igniteLogo.svg";
+import { Timer, Scroll } from "phosphor-react";
+import { NavLink } from "react-router-dom";
 
 export function Header() {
   return (
     <Container>
-      <h1>Header</h1>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
     </Container>
   );
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,3 +1,31 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    gap: 0.8rem;
+
+    a {
+      width: 4.8rem;
+      height: 4.8rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: ${({ theme }) => theme["gray-100"]};
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      &:hover {
+        border-bottom: 3px solid ${({ theme }) => theme["green-500"]};
+      }
+
+      &.active {
+        color: ${({ theme }) => theme["green-500"]};
+      }
+    }
+  }
+`;
