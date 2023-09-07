118a90bbcf29ac9c1e8a60276ca325e729ccec6e
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-06 23:12:57 -0300

    add LayoutContainer

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -1,11 +1,12 @@
 import { Header } from "../../components/Header";
 import { Outlet } from "react-router-dom";
+import { LayoutContainer } from "./style";
 
 export function DefaultLayout() {
   return (
-    <>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </>
+    </LayoutContainer>
   );
 }
