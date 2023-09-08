be14fdeafee43a2e817cc1b35954924a178f020b
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-08 12:38:43 -0300

    add history table

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,50 @@
-import { Container } from "./style";
+import { HistoryContainer, HistoryList } from "./style";
 
 export function History() {
   return (
-    <Container>
-      <h1>History</h1>
-    </Container>
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluída</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluída</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluída</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluída</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   );
 }
--- src/pages/History/style.ts
+++ src/pages/History/style.ts
@@ -1,3 +1,61 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const HistoryContainer = styled.div`
+  flex: 1;
+  padding: 5.6rem;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 2.4rem;
+    color: ${({ theme }) => theme["gray-100"]};
+  }
+`;
+
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+  margin-top: 3.2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+  }
+
+  th {
+    background: ${({ theme }) => theme["gray-600"]};
+    color: ${({ theme }) => theme["gray-100"]};
+    padding: 1.6rem;
+    text-align: left;
+    font-size: 1.4rem;
+    line-height: 1.6;
+
+    &:first-child {
+      border-top-left-radius: 8px;
+      padding-left: 2.4rem;
+    }
+
+    &:last-child {
+      border-top-right-radius: 8px;
+      padding-right: 2.4rem;
+    }
+  }
+
+  td {
+    background: ${({ theme }) => theme["gray-700"]};
+    border-top: 4px solid ${({ theme }) => theme["gray-800"]};
+    padding: 1.6rem;
+    font-size: 1.4rem;
+    line-height: 1.6;
+
+    &:first-child {
+      padding-left: 2.4rem;
+      width: 50%;
+    }
+
+    &:last-child {
+      padding-right: 2.4rem;
+    }
+  }
+`;
